391c7b6ab3f6d71ab3a14f16fdc107925b28a501
KagisoLegodi <156658455+KagisoLegodi@users.noreply.github.com>
2024-04-24 09:25:06 +0200

    use correct id for attaching the event listener

--- escapeVanillaJS.js
+++ escapeVanillaJS.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   // Fixed bug: Correct ID used for attaching the event listener
-  document.getElementById("solveRoom").addEventListener("click", () => {
+  document.getElementById("solveRoom1").addEventListener("click", () => {
     fetch("books.json")
       .then((response) => response.json())
       .then((books) => {
